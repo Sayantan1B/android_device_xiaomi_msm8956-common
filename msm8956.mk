6a0a449086619bde76ec9747281e226f8b74eca5
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-08 19:21:59 -0800

    msm8956-common: Build RCS
    Change-Id: I456aa2db554647f4f6303964f7fb071f98b923ac

--- msm8956.mk
+++ msm8956.mk
@@ -260,6 +260,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# RCS
+PRODUCT_PACKAGES += \
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_api \
+    rcs_service_api.xml
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
