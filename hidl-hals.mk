93683505a4083d11d829a6721de340dd7083ff3e
TheStrechh <TheStrechh@gmail.com>
2018-08-09 21:41:38 +0000

    msm8956-common: Mokee-ify

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -64,7 +64,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
+    vendor.mokee.livedisplay@1.0-service-sdm
 
 # Netutils
 PRODUCT_PACKAGES += \
